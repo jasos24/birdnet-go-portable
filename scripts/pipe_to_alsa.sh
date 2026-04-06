46703a203396bc6cfaf772b84e4d9652d6c6695e
jasos24 <jasonsavino10@gmail.com>
2026-04-06 15:04:31 +1000

    Add files via upload

---
+++ scripts/pipe_to_alsa.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+FIFO_PATH=${1:-"./audio/in.pcm"}
+DEVICE=${2:-"hw:Loopback,0,0"}
+
+ffmpeg -hide_banner -loglevel warning \
+  -f s16le -ar 48000 -ac 1 -i "$FIFO_PATH" \
+  -f alsa "$DEVICE"
