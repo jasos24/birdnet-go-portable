dd81611808464f330ae91213b69ac671884024de
jasos24 <jasonsavino10@gmail.com>
2026-04-10 19:34:01 +1000

    Refactor run_setrtmp function to run and update RTMP URL

--- birdcall/src/commands/setrtmp.rs
+++ birdcall/src/commands/setrtmp.rs
@@ -1,29 +1,15 @@
 use serenity::prelude::*;
 use serenity::model::prelude::*;
-use std::env;
-use std::fs;
+use crate::rtmp;
 
-pub async fn run_setrtmp(ctx: &Context, cmd: &ApplicationCommandInteraction) {
-    let new_url = cmd.data.options[0].value.as_ref().unwrap().as_str().unwrap();
+pub async fn run(ctx: &Context, cmd: &ApplicationCommandInteraction) {
+    let new_url = cmd.data.options[0].value.as_str().unwrap();
 
-    let old = env::var("RTMP_URL").unwrap_or_default();
-    let env_path = ".env";
-
-    if let Ok(mut env_file) = fs::read_to_string(env_path) {
-        if !old.is_empty() {
-            env_file = env_file.replace(
-                &format!("RTMP_URL={}", old),
-                &format!("RTMP_URL={}", new_url),
-            );
-        } else {
-            env_file.push_str(&format!("\nRTMP_URL={}", new_url));
-        }
-        let _ = fs::write(env_path, env_file);
-    }
+    rtmp::set_rtmp_url(new_url);
 
     cmd.create_interaction_response(&ctx.http, |r| {
-        r.interaction_response_data(|m| {
-            m.content("📡 BirdCall has locked onto a new stream destination.")
-        })
-    }).await.ok();
+        r.interaction_response_data(|d| d.content("📡 BirdCall updated the RTMP destination."))
+    })
+    .await
+    .ok();
 }
