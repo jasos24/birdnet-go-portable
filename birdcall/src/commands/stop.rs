30673fbd007555f54b8eed69d04f35640656a878
jasos24 <jasonsavino10@gmail.com>
2026-04-10 19:33:23 +1000

    Update stop.rs

--- birdcall/src/commands/stop.rs
+++ birdcall/src/commands/stop.rs
@@ -2,15 +2,15 @@ use serenity::prelude::*;
 use serenity::model::prelude::*;
 use songbird::SerenityInit;
 
-pub async fn run_stop(ctx: &Context, cmd: &ApplicationCommandInteraction) {
+pub async fn run(ctx: &Context, cmd: &ApplicationCommandInteraction) {
     let guild_id = cmd.guild_id.unwrap();
 
     let manager = songbird::get(ctx).await.unwrap().clone();
     let _ = manager.remove(guild_id).await;
 
     cmd.create_interaction_response(&ctx.http, |r| {
-        r.interaction_response_data(|m| {
-            m.content("🌙 BirdCall spreads its wings and glides out of the channel. Stream ended.")
-        })
-    }).await.ok();
+        r.interaction_response_data(|d| d.content("🌙 BirdCall glides away. Stream ended."))
+    })
+    .await
+    .ok();
 }
