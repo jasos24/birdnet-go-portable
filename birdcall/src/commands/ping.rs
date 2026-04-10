1aab5e9498fd3ef1e497d7df02fdb66a69490a45
jasos24 <jasonsavino10@gmail.com>
2026-04-10 18:15:51 +1000

    Create ping.rs

---
+++ birdcall/src/commands/ping.rs
@@ -0,0 +1,10 @@
+use serenity::prelude::*;
+use serenity::model::prelude::*;
+
+pub async fn run_ping(ctx: &Context, cmd: &ApplicationCommandInteraction) {
+    cmd.create_interaction_response(&ctx.http, |r| {
+        r.interaction_response_data(|m| {
+            m.content("🫧 BirdCall is awake and listening — all systems look good.")
+        })
+    }).await.ok();
+}
